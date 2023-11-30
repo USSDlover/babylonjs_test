1434c6fc791c2f1b9b331b2bc4e780a65b88c21b
Alireza <a.razinejad@gmail.com>
2023-11-30 07:50:36 +0330

    chore: initialize the babylonjs application

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+module.exports = {
+	mode: 'development',
+	entry: [
+		'./src/app/app.ts',
+	],
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				use: [ 'ts-loader' ],
+				exclude: /node_modules/,
+			}
+		],
+	},
+	devServer: {
+		contentBase: path.resolve(__dirname, './public'),
+	},
+	resolve: {
+		extensions: ['.ts', '.js'],
+	},
+};
